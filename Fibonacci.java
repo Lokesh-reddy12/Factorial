8af0c0a7c5975181def42517809f89d90407f070
Lokesh-reddy12 <yelamakurulokesh@gmail.com>
2025-06-04 10:40:44 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+public class Fibonacci
+{
+    static int fibonacci(int n){
+        if(n==0){
+            return 1;
+        }
+        else if(n==1){
+            return 1;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+	public static void main(String[] args) {
+	    int terms=7;
+		System.out.println("fibonacci series");
+		for(int i=0;i<terms;i++){
+		    System.out.println(fibonacci(i)+" ");
+		}
+	}
+}
