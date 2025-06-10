c8bb1a4e4c77144fef2d0f24cc3cf6097b82f352
Lokesh-reddy12 <yelamakurulokesh@gmail.com>
2025-06-10 14:41:12 +0530

    Add files via upload

---
+++ selectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+class selectionSort{
+    static void selectionSort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min_idx=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_idx]){
+                min_idx=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[min_idx];
+        arr[min_idx]=temp;
+    }
+}
+static  void printArray(int[] arr){
+    for(int val:arr){
+        System.out.println(val+" ");
+    }
+    System.out.println();
+}
+public static void main(String[] args){
+    int[] arr={64,25,12,22,11};
+    System.out.println("Original Array:");
+    printArray(arr);
+    selectionSort(arr);
+    System.out.println("Sorted Array:");
+    printArray(arr);
+}}
