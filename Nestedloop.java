8a3d85d45ef2af9785fac4c14f1ce9468578e564
Lokesh-reddy12 <yelamakurulokesh@gmail.com>
2025-06-11 15:37:16 +0530

    Add files via upload

---
+++ Nestedloop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printPairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[i] + " " + arr[j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        printPairs(arr);
+
+        scanner.close();
+    }
+}
